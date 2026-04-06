42b29b89d22e76ec082cb72313d84c3c833ca60d
reenagamregmailcom <reena.gamre@gmail.com>
2026-04-06 21:39:02 +0530

    Show cards in beginning for few seconds

--- Assets/Scripts/GenerateBoard.cs
+++ Assets/Scripts/GenerateBoard.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
+using System.Collections;
 
 public class GenerateBoard : MonoBehaviour
 {
@@ -12,7 +13,8 @@ public class GenerateBoard : MonoBehaviour
    [SerializeField] private RectTransform panelRow;
    [SerializeField] private Button loadMenuSceneButton;
    public Sprite[] icons;
-   List<int> ids;
+   [SerializeField] private List<int> ids;
+   [SerializeField] private List<Card> allCards;
     void Start()
     {
         Button btn = loadMenuSceneButton.GetComponent<Button>();
@@ -43,11 +45,13 @@ public class GenerateBoard : MonoBehaviour
                 int cardId = rowIndex * SetLevel.VerticalLayoutCount + columnIndex;
                 c.id = ids[cardId];
                 c.icon = icons[ids[cardId]];
+                allCards.Add(c);
             }
         }
+        StartCoroutine(ShowCaseCardsInitially());
     }
 
- void Shuffle(List<int> list)
+    void Shuffle(List<int> list)
     {
         for (int i = 0; i < list.Count; i++)
         {
@@ -58,6 +62,20 @@ public class GenerateBoard : MonoBehaviour
         }
     }
 
+   IEnumerator ShowCaseCardsInitially()
+    {
+        for (int i = 0; i < allCards.Count; i++)
+        {
+             allCards[i].Flip();
+        }
+       
+        yield return new WaitForSeconds(3f);
+
+        for (int i = 0; i < allCards.Count; i++)
+        {
+             allCards[i].Hide();
+        }
+    }
 void LoadMenuSceneOnClick()
    {
 		SceneManager.LoadScene(0);
